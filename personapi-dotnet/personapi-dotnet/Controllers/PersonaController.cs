1d82c4319d4759a0771532d8f214aaa1c49e47bb
Johan Mateo Rosero Quenguan <roseroq-j@javeriana.edu.co>
2023-04-27 20:40:33 -0500

    planteamiento inicial

---
+++ personapi-dotnet/personapi-dotnet/Controllers/PersonaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Models.Entities;
+
+namespace personapi_dotnet.Controllers
+{
+    public class PersonaController : Controller
+    {
+        private readonly PersonaDbContext _context;
+
+        public PersonaController(PersonaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _context.Personas.ToListAsync());
+        }
+    }
+}
